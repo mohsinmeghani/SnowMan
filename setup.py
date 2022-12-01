c3335c67a37ab848298b9738e73dad67682a1e32
mohsinmeghani <meghani.mohsin@gmail.com>
2022-12-01 17:46:47 +0100

    changes to setup and init file

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from distutils.command import install_data
+from typing_extensions import Required
+from setuptools import setup, find_packages
 
 setup(
-   name='SnowMan',
+   name='snowflake',
    version='1.0',
    description='A useful module',
    author='Mohsin',
    author_email='meghani.mohsin@gmail.com',
-   packages=[]
+   packages=find_packages(),
+   install_requires= Required
 )
---
+++ snowflake/__init__.py
@@ -0,0 +1,4 @@
+import imp
+from snowflake.let_it_snow import *
+
+main()
