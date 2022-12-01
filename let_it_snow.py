a8091548af82efafa346ac34038365baf5e7887c
mohsinmeghani <meghani.mohsin@gmail.com>
2022-12-01 16:28:12 +0100

    Update let_it_snow.py
    -Add Random color on flakes
    -Add  Exit on Click on turtle for screen halt

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,5 +1,6 @@
 import turtle
 import numpy as np
+import time
 
 
 def main(speed=0, bg_color="grey"):
@@ -11,28 +12,36 @@ def main(speed=0, bg_color="grey"):
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
+   
+
     """TODO: define different colors here"""
-
-
+    mycolors= ["red","purple","orange","green","yellow","pink","black","white","cyan","magenta"]
+    
+    myTurtle.screen.colormode(255)
+    
+    
     for _ in range(10):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
-
+        
         """TODO: set snowflake color here (one of the colors defined above)"""
+        myTurtle.color(mycolors[_])
 
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
         myTurtle.pendown()
+        
+        
 
         # draw the snowflake
         for _ in range(8):
             snowflake_branch(size, myTurtle)
             myTurtle.left(45)
+    turtle.exitonclick()    
 
+    
 
 def snowflake_branch(size, myTurtle):
     # This function draws one branch of the snowflake.
